ba4c0ed5c70a1c0c2ea629905235b57efb68addf
Florent Brassart <florent.brassart.partner@decathlon.com>
2021-04-04 13:03:34 +0200

    Redirect to error page if bill is unsuccesful

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends AbstractController
+{
+    #[Route('/error', name: 'error')]
+    public function show(): Response
+    {
+        return $this->render('error.html.twig');
+    }
+}
